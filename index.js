c75d304a78170aaccdddc9552060aac23b68f8e3
vinay 3elitist <vinay.3elitist@gmail.com>
2024-05-31 12:49:07 +0530

    updated index.js

--- index.js
+++ index.js
@@ -24,14 +24,12 @@ function newFeatureAdd2(){
     console.log("New Feature4");
 }
 
-<<<<<<< Updated upstream
 function newFeatureAdd3(){
     console.log("New Feature2");
     console.log("New Feature3");
     console.log("New Feature4");
 }
-=======
+
 function newFeatureAdd2(){
     console.log("New Feature");
 }
->>>>>>> Stashed changes
